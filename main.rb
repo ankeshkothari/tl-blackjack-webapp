3bd5e35f428871dda5331645992012cc2256fbc3
Ankesh Kothari <ankeshk@ankeshkothari.com>
2015-07-30 17:13:25 +0530

    downloaded files and added Procfile

---
+++ main.rb
@@ -0,0 +1,12 @@
+require 'rubygems'
+require 'sinatra'
+
+use Rack::Session::Cookie, :key => 'rack.session',
+                           :path => '/',
+                           :secret => 'ankesh' 
+
+
+
+get '/' do
+  "Hello World"
+end
